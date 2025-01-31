6dcc780c6fe57b787d41a062e56c535ba7973eac
hp <b1779295@live.tees.ac.uk>
2025-01-31 20:42:49 +0000

    service section added

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'custom-gradient': 'linear-gradient(180deg, rgb(255, 255, 255), rgb(239, 235, 227) 100%)',
+        'card-bg':'rgb(239, 235, 227)',
+      }
+    }
+  }
+}
